f1aac1f298efe8c48ebbd7ee80cdec152579477d
shyunju7 <shyunju7@naver.com>
2021-10-22 14:03:40 +0900

    Add Sticker Component

--- rolling-paper-app/src/components/Sticker/style.ts
+++ rolling-paper-app/src/components/Sticker/style.ts
@@ -4,4 +4,8 @@ export const StickerContainer = styled.div`
   width: 300px;
   height: 300px;
   background-color: lightpink;
+  position: absolute;
+  top: 0;
+  left: 0;
+  cursor: pointer;
 `;
--- rolling-paper-app/src/pages/Main.tsx
+++ rolling-paper-app/src/pages/Main.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import Sticker from "../components/Sticker";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: #ffff0f;
+`;
+
 const Main = () => {
   return (
-    <div>
+    <Container>
       <h1>MAIN</h1>
       <Sticker />
-    </div>
+    </Container>
   );
 };
 
