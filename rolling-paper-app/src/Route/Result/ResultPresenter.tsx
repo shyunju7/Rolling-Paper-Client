09d888f8ed2f8e458ad2d32c66ff520ed360526e
shyunju7 <shyunju7@naver.com>
2021-11-12 20:28:07 +0900

    Feat ResultPage button

--- rolling-paper-app/src/Route/Result/ResultPresenter.tsx
+++ rolling-paper-app/src/Route/Result/ResultPresenter.tsx
@@ -5,7 +5,11 @@ const ResultPresenter = ({ userName }: any) => {
     <div>
       <Header userName={userName} text="님의 롤링페이퍼" />
 
-      <Container>결과</Container>
+      <Container>
+        결과
+        <button>결과 공유</button>
+        <button>다운로드</button>
+      </Container>
     </div>
   );
 };
