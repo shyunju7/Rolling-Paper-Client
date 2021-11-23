f207f6d6e3666700dfacbc3aaf0a9d14c059b7a0
shyunju7 <shyunju7@naver.com>
2021-11-23 14:46:11 +0900

    Style font

--- rolling-paper-app/src/GlobalStyles.ts
+++ rolling-paper-app/src/GlobalStyles.ts
@@ -3,8 +3,28 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
 
-
 ${reset}
+
+@font-face {
+  font-family: "HSYuji";
+  src: url("../src/font/HSYuji.ttf") format("woff"),
+    url("../src/font/HSYuji.ttf") format("woff2");
+}
+
+@font-face {
+  font-family: "nanum";
+  src: url("../src/font/나눔손글씨\ 왼손잡이도\ 예뻐.ttf") format("woff"),
+    url("../src/font/나눔손글씨\ 왼손잡이도\ 예뻐.ttf") format("woff2");
+}
+
+@font-face {
+  font-family: "Uiyeun";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105@1.1/Uiyeun.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
 a {
         text-decoration : none;
         color : inherit;
